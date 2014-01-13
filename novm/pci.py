050e2c7dad97e450106e042da7a5b596c6ccc9dc
Adin Scannell <adin@scannell.ca>
2014-01-13 17:00:58 -0500

    Force PCI mode.

--- novm/pci.py
+++ novm/pci.py
@@ -7,6 +7,16 @@ class PciBus(device.Device):
 
     driver = "pci-bus"
 
+    def cmdline(self):
+        return "pci=conf1"
+
 class PciHostBridge(device.Device):
+
+    # NOTE: For now, PCI support is pretty sketchy.
+    # Generally, we'll need to have a hostbridge appear
+    # in the list of devices. This is all voodoo to me,
+    # so for now the problem is solved quite simply by
+    # passing a command line parameter above that forces
+    # appropriate PCI detection.
 
     driver = "pci-hostbridge"
