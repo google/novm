67f66a649627f83432b68ac78c48342b8f96018a
Adin Scannell <ascannell@google.com>
2014-07-18 14:24:07 -0400

    Add appropriate per-file license headers.

--- src/novmm/platform/kvm_run.h
+++ src/novmm/platform/kvm_run.h
@@ -7,6 +7,20 @@
  * scheduling and the ability to deliver a signal to a specific
  * thread. Because we are not able to control this from Go,
  * we need to isolate some calls in C for pause/unpause control.
+ *
+ * Copyright 2014 Google Inc. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 
 #include <pthread.h>
