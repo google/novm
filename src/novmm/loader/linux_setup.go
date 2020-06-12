b6502d6852457427624b5dd884eb4e1716750bfc
Chris Olstrom <chris@olstrom.com>
2020-06-12 10:41:35 -0700

    Fix struct member names on modern Linuxes
    On 2017-01-27, in `struct boot_params`, member `e820_map` was renamed twice, initially
    to `e820_array` and subsequently to `e820_table`.
        
    References:
    - https://github.com/torvalds/linux/commit/acd4c048728814505fae8e224cf9074bd1ad291e
    - https://github.com/torvalds/linux/commit/61a50101638254d38e3f4281265b44de0f2cba4e
        
    This resolves the following build failures:
    ```
    src/novmm/loader/linux_setup.go: In function 'e820_set_region':
    src/novmm/loader/linux_setup.go:44:9: error: 'struct boot_params' has no member named 'e820_map'
         boot->e820_map[index].addr = start;
             ^
    src/novmm/loader/linux_setup.go:45:9: error: 'struct boot_params' has no member named 'e820_map'
         boot->e820_map[index].size = size;
             ^
    src/novmm/loader/linux_setup.go:46:9: error: 'struct boot_params' has no member named 'e820_map'
         boot->e820_map[index].type = type;
             ^
    ```

--- src/novmm/loader/linux_setup.go
+++ src/novmm/loader/linux_setup.go
@@ -41,9 +41,9 @@ static inline void e820_set_region(
     __u64 size,
     __u8 type) {
 
-    boot->e820_map[index].addr = start;
-    boot->e820_map[index].size = size;
-    boot->e820_map[index].type = type;
+    boot->e820_table[index].addr = start;
+    boot->e820_table[index].size = size;
+    boot->e820_table[index].type = type;
 }
 static inline void set_header(
     struct boot_params* boot,
